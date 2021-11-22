3f3a20d978c2d4bf209ad1e803f2a389642aa003
Martin <1765070265@qq.com>
2021-11-22 18:37:16 +0800

    en

--- git/src/com/xxx/test/FirstClass.java
+++ git/src/com/xxx/test/FirstClass.java
@@ -3,9 +3,11 @@ package com.xxx.test;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git");
+        //hello test
     }
     public void a(){
         int x= 0 ;
+        System.out.println(x);
     }
 
 }
